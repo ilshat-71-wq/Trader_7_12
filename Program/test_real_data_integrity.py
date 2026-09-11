eca1fb94111eab2d8097768939449f9180d145b9
ilshat-71-wq <ilshat-71@mail.ru>
2026-09-11 21:04:40 +0300

    Fix real-data integrity test double

--- Program/test_real_data_integrity.py
+++ Program/test_real_data_integrity.py
@@ -1,14 +1,18 @@
 from services.futures_oi_marketdata_scanner_service import FuturesOIMarketDataScannerService
 
 
+class _FakeOI:
+    TIMEOUT = 15
+
+
 def test_missing_underlying_is_not_available():
-    service = FuturesOIMarketDataScannerService(api=object(), oi_service=object())
+    service = FuturesOIMarketDataScannerService(api=object(), oi_service=_FakeOI())
     service._underlying_class_codes = {}
     assert service._underlying_day_change("UNKNOWN") == (None, "NO_CLASS_CODE")
 
 
 def test_missing_base_data_cannot_be_reported_as_available():
-    service = FuturesOIMarketDataScannerService(api=object(), oi_service=object())
+    service = FuturesOIMarketDataScannerService(api=object(), oi_service=_FakeOI())
     service._underlying_class_codes = {}
     value, source = service._underlying_day_change("SBER")
     assert value is None
